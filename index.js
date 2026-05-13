e814a6002cc57575801f33c59c0686d7c70e7508
eduardodiazpompa221104-ops <eduardodiazpompa221104@gmail.com>
2026-05-13 16:40:18 -0600

    conexion a base de datos

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from "express"
 import router from "./routes/inicio_router.js"
-
+import db from "./config/db.js"
 //Crear la aplicación
 const app = express()
 
 //accesos a los datos del formulario
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
+
+//Conexión a la base de datos
+try {
+  await db.authenticate();
+  db.sync()
+  console.log("Conexion exitosa a la base de datos");
+} catch (error) {
+  console.log(error);
+}
 
 //routing
 app.use("/", router)
